c80d89c080ba9920e4215cc7bf3598d5864084cc
thanhnld0912 <joshlemuel241291@gmail.com>
2024-10-25 10:05:08 +0700

    Delete top rating, top wishlist; change admin id in add comment

--- src/main/java/Controller/AddComment.java
+++ src/main/java/Controller/AddComment.java
@@ -23,7 +23,7 @@ public class AddComment extends HttpServlet {
                         throws ServletException, IOException {
                 // Retrieve the form parameters
                 String comment = request.getParameter("comment");
-                String memberId = request.getParameter("memberId");
+                String userId = request.getParameter("memberId");
                 String topicId = request.getParameter("topicid");
 
                 LocalDateTime currentDateTime = LocalDateTime.now();
@@ -33,7 +33,7 @@ public class AddComment extends HttpServlet {
                 MongoCollection<Document> collection = database.getCollection("comment");
 
                 Document comments = new Document("TopicId", topicId)
-                                .append("UserId", memberId)
+                                .append("UserId", userId)
                                 .append("Content", comment)
                                 .append("Status", "unedited")
                                 .append("Date", currentDateTime);
